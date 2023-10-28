df659e2008e1d0a14b35050fe867c0c379128eda
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2023-10-28 12:47:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//name space
+namespace RandomNumberGuesser
+{
+    //main class
+    class program
+    {
+        //entry point methods
+        static void Main(string[] args)
+        {
+            //String name = "prabod pubudu";
+            //int age = 23;
+
+            //start here//
+            //Console.WriteLine("hello world "+ name);
+            //Console.WriteLine("{0} is {1}",name,age);
+
+
+            // set app vars
+            String appname = "Number Guesser";
+            String appversion = "1.0.0";
+            String appAuthor = "pubudu";
+
+            //str color for app name and version well
+            Console.ForegroundColor= ConsoleColor.DarkRed;
+
+            //print app info
+            Console.WriteLine($"{appname}: version{appversion} by {appAuthor} ");
+
+            //reset app color
+            Console.ResetColor();
+
+
+
+
+
+
+        }
+    }
+}
+
